3f41f9239dcdd3b7db6c85acceec0946230c6883
imbvn <103633807+imbvn@users.noreply.github.com>
2023-11-01 04:19:51 -0400

    Added functionality to buttons

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+let color = 'black';
 
 function createGrid(size) {
     const container = document.querySelector('.grid-container');
@@ -11,10 +11,30 @@ function createGrid(size) {
 
     for (let i = 0; i < size ** 2; i++) {
         const square = document.createElement('div');
-        square.addEventListener('mouseover', () => square.style.backgroundColor = 'black');
+        square.addEventListener('mouseover', colorSquare);
         square.style.background = 'white';
         container.insertAdjacentElement('beforeend', square);
     }
+}
+
+function colorSquare() {
+    if (color === 'rainbow') {
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    }
+    else {
+        this.style.backgroundColor = color;
+    }
+}
+
+function changeColor(choice) {
+    color = choice;
+}
+
+function resetBoard() {
+    const container = document.querySelector('.grid-container');
+    let squares = container.querySelectorAll('div');
+    
+    squares.forEach((div) => div.style.backgroundColor = 'white');
 }
 
 createGrid(16)
